4960e5a1256b90591f146cb4945b43a6947bfe02
andromeda <q.a.r.h.hrp0204@gmail.com>
2025-03-18 09:00:13 +0700

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const short = document.getElementById('short');
+
+function bars(){
+    short.style.transform = 'translateY(0)';
+}
+
+function cancel(){
+    short.style.transform = 'translateY(-10000px)';
+}
